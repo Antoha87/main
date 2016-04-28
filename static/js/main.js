b5416ea83e3b6b09323a561bd9dc48e950b87626
Melvin-88 <taisen747@gmail.com>
2016-04-28 21:46:30 +0300

    menu

--- static/js/main.js
+++ static/js/main.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
 
     $('.item_container').on('click', function(e){
         e.preventDefault();
+        console.log($(this));
         $('.item_container').each(function(){
             if($(this).hasClass('selected')){
                 $(this).removeClass('selected');
@@ -12,23 +13,37 @@ $(document).ready(function() {
 
     var liSelected = $('nav .main_menu .popup_menu .popup_menu_table .main_rubrics li');
 
-    liSelected.on('mouseover', function(e){
+    liSelected.on('click', function(e){
         e.preventDefault();
 
         var idLi = $(this).attr("id");
 
-        liSelected.each(function(){
-            if($(this).hasClass('selected')){
-                $(this).removeClass('selected');
-            }
-        })
+        function delSelect (){
+            liSelected.each(function(){
+                if($(this).hasClass('selected')){
+                    $(this).removeClass('selected');
+                }
+            })
+        }
+        delSelect();
 
         $('.sub_rubric_container > div').each(function(){
-                    $(this).hide();
+            $(this).hide();
         });
 
         $('div.'+idLi).show();
 
         $(this).addClass('selected');
     })
+
+    $(document).click( function(event){
+        if( $(event.target).closest(".main_menu").length ) return;
+        $('.item_container').each(function(){
+            if($(this).hasClass('selected')){
+                $(this).removeClass('selected');
+            }
+        });
+        event.stopPropagation();
+    });
+
 });
