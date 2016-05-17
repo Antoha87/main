122710183b9abd3e6257d295cd32c8fd930aae51
Antoha87 <mak.anton87@gmail.com>
2016-05-17 17:31:11 +0300

    fix

--- catalog/migrations/0029_goods_slug.py
+++ catalog/migrations/0029_goods_slug.py
@@ -18,7 +18,7 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='goods',
             name='slug',
-            field=models.SlugField(default=slugify(datetime.datetime.now()), max_length=200, unique=True, verbose_name='\u0427\u041f\u0423'),
+            field=models.SlugField(default=slugify(datetime.datetime.now().strftime), max_length=200, unique=True, verbose_name='\u0427\u041f\u0423'),
             preserve_default=False,
         ),
     ]
