5ecd237209db892b954e905210df511c757d8416
Antoha87 <mak.anton87@gmail.com>
2016-05-17 17:36:12 +0300

    uuid

--- catalog/migrations/0029_goods_slug.py
+++ catalog/migrations/0029_goods_slug.py
@@ -6,7 +6,9 @@ import datetime
 from django.db import migrations, models
 from django.utils.timezone import utc
 from slugify import slugify
-import random
+import uuid
+
+
 
 class Migration(migrations.Migration):
 
@@ -18,7 +20,7 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='goods',
             name='slug',
-            field=models.SlugField(default=slugify(random.choice("qwertyuiopasdfghjklzxcvbnm")), max_length=200, unique=True, verbose_name='\u0427\u041f\u0423'),
+            field=models.SlugField(default=slugify(uuid.uuid4()), max_length=200, unique=True, verbose_name='\u0427\u041f\u0423'),
             preserve_default=False,
         ),
     ]
