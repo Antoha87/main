ef1358f8a8dc04362cd384fd4dd65eeff0fec1e5
Antoha87 <mak.anton87@gmail.com>
2016-07-25 05:26:11 +0300

    add category

---
+++ static/js/main.js
@@ -0,0 +1,71 @@
+$(document).ready(function() {
+
+    $('.item_container').on('click', function(e){
+        //e.preventDefault();
+        $('.item_container').each(function(){
+            if($(this).hasClass('selected')){
+                $(this).removeClass('selected');
+            }
+        })
+        $(this).addClass('selected');
+    })
+
+    var liSelected = $('nav .main_menu .popup_menu .popup_menu_table .main_rubrics li');
+
+    liSelected.on('click', function(e){
+        e.preventDefault();
+
+        var idLi = $(this).attr("id");
+
+        function delSelect (){
+            liSelected.each(function(){
+                if($(this).hasClass('selected')){
+                    $(this).removeClass('selected');
+                }
+            })
+        }
+        delSelect();
+
+        $('.sub_rubric_container > div').each(function(){
+            $(this).hide();
+        });
+
+        $('div.'+idLi).show();
+
+        $(this).addClass('selected');
+    })
+
+    $(document).click( function(event){
+        if( $(event.target).closest(".main_menu").length ) return;
+        $('.item_container').each(function(){
+            if($(this).hasClass('selected')){
+                $(this).removeClass('selected');
+            }
+        });
+        event.stopPropagation();
+    });
+
+
+    $('.icon_times_drop').on('click', function(e){
+        e.preventDefault();
+        console.log('11');
+        $('.drop-enter').css('display','none');
+        $('.for-center').css('display','none');
+        $('.overlayDrop').css('display','none');
+    })
+
+    $('.for-center').on('click', function(e){
+        e.preventDefault();
+        console.log('11');
+        $('.drop-enter').css('display','none');
+        $('.for-center').css('display','none');
+        $('.overlayDrop').css('display','none');
+    })
+
+});
+
+
+
+
+
+
